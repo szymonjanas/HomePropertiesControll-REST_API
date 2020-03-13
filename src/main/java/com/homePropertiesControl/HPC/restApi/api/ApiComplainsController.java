1c52b00c3c0d53795e326b52fc0aa84f7962f745
szymon <szymon.janas.95@gmail.com>
2020-03-13 22:21:35 +0100

    Add: complains controller

---
+++ src/main/java/com/homePropertiesControl/HPC/restApi/api/ApiComplainsController.java
@@ -0,0 +1,33 @@
+package com.homePropertiesControl.HPC.restApi.api;
+
+import com.homePropertiesControl.HPC.restApi.Repository.ComplainsRepository;
+import com.homePropertiesControl.HPC.restApi.model.Complains;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/complains")
+public class ApiComplainsController {
+
+    @Autowired
+    private ComplainsRepository complainsRepository;
+
+    @PutMapping
+    public String putComplainToDatabase(@RequestBody String complain){
+        JSONObject jsonObject = new JSONObject(complain);
+        JSONObject response = new JSONObject();
+        String id = UUID.randomUUID().toString();
+        complainsRepository.save(new Complains( id,
+                                                jsonObject.getString("type"),
+                                                jsonObject.getString("message") ));
+        response.put("message", "ok");
+        return response.toString();
+    }
+
+}

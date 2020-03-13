714755d3777b4c65b83983b9a19698280942ee9c
szymon <szymon.janas.95@gmail.com>
2020-03-13 19:55:35 +0100

    Add: complains model

---
+++ src/main/java/com/homePropertiesControl/HPC/restApi/model/Complains.java
@@ -0,0 +1,66 @@
+package com.homePropertiesControl.HPC.restApi.model;
+
+import org.json.JSONObject;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Iterator;
+
+@Entity
+public class Complains {
+
+    @Id
+    private String id;
+    private String type;
+    private String content;
+
+    public Complains() {}
+
+    public Complains(String id, String type, String content) {
+        this.id = id;
+        this.type = type;
+        this.content = content;
+    }
+
+    public Complains(JSONObject jsonObject) {
+        Iterator iter = jsonObject.keys();
+        while(iter.hasNext()){
+            String key = (String) iter.next();
+            switch (key){
+                case "id":
+                    this.id =  (String) jsonObject.get(key);
+                    break;
+                case "type":
+                    this.type =  (String) jsonObject.get(key);
+                    break;
+                case "content":
+                    this.content =  (String) jsonObject.get(key);
+                    break;
+            }
+        }
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}

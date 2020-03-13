a4a15a0cff99d74cd8678376b711f6021dcb18fa
szymon <szymon.janas.95@gmail.com>
2020-03-13 21:58:51 +0100

    Refactor: removed unused import

--- src/main/java/com/homePropertiesControl/HPC/restApi/api/ApiMotdController.java
+++ src/main/java/com/homePropertiesControl/HPC/restApi/api/ApiMotdController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Random;
 
 @RestController

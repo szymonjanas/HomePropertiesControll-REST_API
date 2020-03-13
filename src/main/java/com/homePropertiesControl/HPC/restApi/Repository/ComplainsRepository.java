ed8e8853331367b0f0e4f55aa15c7bb3c700c3af
szymon <szymon.janas.95@gmail.com>
2020-03-13 21:12:23 +0100

    Add: repository for complains and motd

---
+++ src/main/java/com/homePropertiesControl/HPC/restApi/Repository/ComplainsRepository.java
@@ -0,0 +1,7 @@
+package com.homePropertiesControl.HPC.restApi.Repository;
+
+import com.homePropertiesControl.HPC.restApi.model.Complains;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComplainsRepository extends CrudRepository<Complains, String> {
+}
---
+++ src/main/java/com/homePropertiesControl/HPC/restApi/Repository/MotdRepository.java
@@ -0,0 +1,7 @@
+package com.homePropertiesControl.HPC.restApi.Repository;
+
+import com.homePropertiesControl.HPC.restApi.model.Motd;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MotdRepository extends CrudRepository<Motd, String> {
+}

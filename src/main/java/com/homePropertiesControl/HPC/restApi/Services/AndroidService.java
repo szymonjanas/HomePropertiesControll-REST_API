c71cec17b25b1c454e61dc1a9049401f9fbb41f9
szymon <szymon.janas.95@gmail.com>
2020-05-07 13:21:26 +0200

    Add: AndroidService

---
+++ src/main/java/com/homePropertiesControl/HPC/restApi/Services/AndroidService.java
@@ -0,0 +1,60 @@
+package com.homePropertiesControl.HPC.restApi.Services;
+
+import com.homePropertiesControl.HPC.restApi.Repository.SensorsRepository;
+import com.homePropertiesControl.HPC.restApi.model.Sensor;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AndroidService {
+
+    private final SensorsRepository sensorsRepository;
+
+    public AndroidService(SensorsRepository sensorsRepository) {
+        this.sensorsRepository = sensorsRepository;
+    }
+
+    public Iterable<Sensor> getAllSensors() {
+        return sensorsRepository.findAll();
+    }
+
+    public String isUserAuthorized() {
+        return "TRUE";
+    }
+
+    public String changeSensor(String command, String sensorStr) {
+        JSONObject jsonObject = new JSONObject(sensorStr);
+        Sensor sensor = new Sensor(jsonObject);
+
+        JSONObject response = new JSONObject();
+        if (sensorsRepository.findById(sensor.getId()).isPresent()){
+            Sensor tsensor = sensorsRepository.findById(sensor.getId()).get();
+            switch (command) {
+                case "level":
+                    tsensor.setLevel(sensor.getLevel());
+                    break;
+                case "name":
+                    tsensor.setName(sensor.getName());
+                    break;
+                case "type":
+                    tsensor.setType(sensor.getType());
+                    break;
+                case "location":
+                    tsensor.setLocation(sensor.getLocation());
+                    break;
+                case "state":
+                    tsensor.setState(sensor.getState());
+                    break;
+                default:
+                    response.put("message", "Command not found!");
+                    return response.toString();
+            }
+            sensorsRepository.save(tsensor);
+            response.put("message", "ok");
+            return response.toString();
+        }
+        response.put("message", "Sensor with given id not found!");
+        return response.toString();
+    }
+
+}
--- src/main/java/com/homePropertiesControl/HPC/restApi/api/ApiAndroidController.java
+++ src/main/java/com/homePropertiesControl/HPC/restApi/api/ApiAndroidController.java
@@ -1,9 +1,8 @@
 package com.homePropertiesControl.HPC.restApi.api;
 
-import com.homePropertiesControl.HPC.restApi.Repository.SensorsRepository;
+import com.homePropertiesControl.HPC.restApi.Services.AndroidService;
 import com.homePropertiesControl.HPC.restApi.model.Sensor;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,57 +10,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/android")
 public class ApiAndroidController {
 
-    @Autowired
-    private SensorsRepository sensorsRepository;
+    private final AndroidService androidService;
+
+    public ApiAndroidController(AndroidService androidService) {
+        this.androidService = androidService;
+    }
 
     @GetMapping
     @PreAuthorize("hasAnyRole('ROLE_ANDROID')")
     public @ResponseBody
     Iterable<Sensor> getAllSensors() {
-        return sensorsRepository.findAll();
+        return androidService.getAllSensors();
     }
 
     @GetMapping("/login")
     @PreAuthorize("hasAnyRole('ROLE_ANDROID')")
     public String isUserAuthorized() {
-        return "TRUE";
+        return androidService.isUserAuthorized();
     }
 
     @PutMapping(path = "/{command}")
     @PreAuthorize("hasAnyRole('ROLE_ANDROID')")
-    public String changeSensor(@PathVariable("command") String command, @RequestBody String sensorStr) {
-
-        JSONObject jsonObject = new JSONObject(sensorStr);
-        Sensor sensor = new Sensor(jsonObject);
-
-        JSONObject response = new JSONObject();
-        if (sensorsRepository.findById(sensor.getId()).isPresent()){
-            Sensor tsensor = sensorsRepository.findById(sensor.getId()).get();
-            switch (command) {
-                case "level":
-                    tsensor.setLevel(sensor.getLevel());
-                    break;
-                case "name":
-                    tsensor.setName(sensor.getName());
-                    break;
-                case "type":
-                    tsensor.setType(sensor.getType());
-                    break;
-                case "location":
-                    tsensor.setLocation(sensor.getLocation());
-                    break;
-                case "state":
-                    tsensor.setState(sensor.getState());
-                    break;
-                default:
-                    response.put("message", "Command not found!");
-                    return response.toString();
-            }
-            sensorsRepository.save(tsensor);
-            response.put("message", "ok");
-            return response.toString();
-        }
-        response.put("message", "Sensor with given id not found!");
-        return response.toString();
+    public String changeSensor(@PathVariable("command") String command, @RequestBody String sensorStr){
+        return androidService.changeSensor(command, sensorStr);
     }
+
 }
